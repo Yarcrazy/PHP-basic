623c2e8ee79da0dd5014cd1fff8987f0607d2928
Yarcrazy <yacrazy@yandex.ru>
2018-10-14 01:45:13 +0700

    Lesson 3 HW 4

--- index.php
+++ index.php
@@ -4,16 +4,15 @@ function getTransLitString($str) {
   $alphabet = [
     'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'jo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й'
     => 'jj', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
-    'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '\'\'', 'ы' => 'y', 'ь' => '\'',
-    'э' => 'eh', 'ю' => 'ju', 'я' => 'ja',
+    'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'с','ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '\'\'', 'ы' => 'y',
+    'ь' => '\'', 'э' => 'e', 'ю' => 'u', 'я' => 'ja',
   ];
   $returnStr = '';
-  $array = mb_split('//', $str, 1);
-  var_dump($array);
+  $array = preg_split('//u', $str);
   foreach ($array as $char) {
-    $returnStr = in_array($char, $alphabet) ? $returnStr . $alphabet[$char] : $returnStr . $char;
+    $returnStr = in_array($char, array_keys($alphabet)) ? $returnStr . $alphabet[$char] : $returnStr . $char;
   }
   return $returnStr;
 }
 
-echo getTransLitString('привет! это тестовый пример для реализации транслитерации строк. к сожалению символы в верхнем регистре пока не поддерживаются');
+echo getTransLitString('привет! это тестовый пример для реализации транслитерации строк');
