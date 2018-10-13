0375db99595b64ee8dbfe6fa096630bdee52d9d5
Yarcrazy <yacrazy@yandex.ru>
2018-10-13 22:36:58 +0700

    Lesson 2 HW 7

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
 
-function getDeclHour(num) {
-  switch (num) {
-    case 0:
-    case 
+function getDeclination($num, $words) {
+  switch ($num % 10) {
+    case 1:
+      $word = $words[0];
+      break;
+    case 2:
+    case 3:
+    case 4:
+      $word = $words[1];
+      break;
+    default:
+      $word = $words[2];
   }
+  if ((($num % 100) > 10) && (($num % 100) < 20)) {
+    $word = $words[2];
+  }
+  return $word;
 }
+
+$words = [
+  'час' => ['час', 'часа', 'часов'],
+  'минута' => ['минута', 'минуты', 'минут'],
+];
+
+$hour = date('H');
+$min = date('i');
+
+echo $hour . ' ' . getDeclination($hour, $words['час']) . ' ';
+echo $min . ' ' . getDeclination($min, $words['минута']) . " \n";
